d48de0414559f1233c7a1206f4bc31eae4cfd6ac
aniketsharma953 <sharmaniket593@gmail.com>
2024-02-18 20:01:02 +0530

    addfiles

---
+++ contact.php
@@ -0,0 +1,211 @@
+<?php
+
+if(isset($_POST['save']))
+{
+	
+$fname=$_POST['fname'];
+
+$lname=$_POST['lname'];
+
+$msg=$_POST['msg'];
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="travelvista";
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+
+
+if($conn->connect_error)
+{
+	die("connection failed".$conn->connect_error);
+}
+
+
+$sql="insert into contactus(fname,lname,msg)values('$fname','$lname','$msg')";
+if($conn->query($sql)==True)
+{
+	?><h3 class="msg"><?php echo "Record inserted successfully";
+	
+	?></h3><?php
+	header("Location: index.html");
+	
+}
+else
+{
+	?><h3 class="msg"><?php echo "Error".$sql."<br/>".$conn->error;?></h3><?php
+}
+$conn->close();
+}
+
+?>
+
+
+
+
+<html>
+<head>
+<title>Contact</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--<link rel="stylesheet" type="text/css" href="bootstrap.css">-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<style>
+a#vis {
+  color: white;
+}
+
+a#vis:hover {
+  color: powderblue;
+}
+.scrolled {
+  background-color: rgba(37, 125, 199, 1) !important;
+}
+</style>
+<body style="background-color:#257DC7">
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top" style="background:rgba(0,0,0,0);">
+<div class="container">
+<a href="#" class="navbar-brand"><img src="lg.png">TRAVEL VISTA</a>
+<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarid">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarid">
+<ul class="navbar-nav text-center ml-auto">
+<li class="nav-items">
+<a href="index.html" class="nav-link" id="vis">Home</a>
+</li>
+<li class="nav-items">
+<a href="about.html" class="nav-link" id="vis">About Us</a>
+</li>
+<li class="nav-items">
+<a href="servecies.html" class="nav-link" id="vis">Services</a>
+</li>
+
+
+<li class="nav-items">
+<a href="login.php" class="nav-link" id="vis">login</a>
+</li>
+<li class="nav-items">
+<a href="signup.php" class="nav-link" id="vis">signup</a>
+</li>
+<li class="nav-items">
+<a href="contact.php" class="nav-link" id="vis">Mail Us</a>
+</li>
+</ul>
+</div>
+</div>
+</nav><br><br><br>
+<div class="container-fluid">
+  <div class="col-xl-12">
+    <div style="height:700px; width:100%; max-width:500px; border-radius: 5px; background-color:#257DC7; margin:6% auto 0; padding:20px;">
+      <h1 style="text-align:center; color:white;">Contact us</h1>
+
+<form method="POST">
+  <label for="fname" style="color:white;">First Name</label>
+  <input type="text" id="fname" name="fname" style="width: 100%; padding: 12px 20px; background-color:#257DC7; margin: 8px 0; box-sizing: border-box; border: none; border-bottom: 2px solid white;" required>
+  <label for="lname" style="color:white;">Last Name</label>
+  <input type="text" id="lname" name="lname" style="width: 100%; padding: 12px 20px; background-color:#257DC7; margin: 8px 0; box-sizing: border-box; border: none; border-bottom: 2px solid white;" required> 
+  <label for="message" style="color: white;">Message</label>
+  <textarea id="message" name="msg" style="width: 100%; height: 150px; background-color: #257DC7; margin: 8px 0; box-sizing: border-box; border: none; border-bottom: 2px solid white;" required></textarea>
+  <center>
+   <input type="submit" name="save" value="Send your Message" style="width: 300px; background-color:white; ; color:#257DC7; padding: 14px 20px; margin: 20px 0; border: none; border-radius: 10px;">
+   </center>
+</form>
+</div>
+</div>
+</div>
+
+<footer class="footer" style="background-color:#257DC7; margin-top:40px">
+<div class="container-fluid" style="padding-top:40px;">
+<center>
+<div class="row">
+<div class="col-xl-3">
+<div style="width:250px height:350px">
+<img width="200px" height="200px" src="logo.png">
+</div>
+</div>
+<div class="col-xl-2">
+<div style="width:250px height:350px">
+<h4 style="color:white; text-align:left;">Get Started</h4>
+<ul style="list-style-type:none; text-align:left;">
+<li><a style="color:white; text-decoration:none; hover=color:black;" href="navcol.html">Home</a></li>
+<li><a style="color:white; text-decoration:none;" href="formdiv.html">Sing Up</a></li>
+<li><a style="color:white; text-decoration:none;" href="servecies.html">Gallery</a></li>
+</ul>
+</div>
+</div>
+<div class="col-xl-2">
+<div style="width:250px height:350px">
+<h4 style="color:white; text-align:left;">About Us</h4>
+<ul style="list-style-type:none; text-align:left;">
+<li><a style="color:white; text-decoration:none;"href="#company information">Company Information</a></li>
+<li><a style="color:white; text-decoration:none;"href="#contact Us">Contact Us</a></li>
+<li><a style="color:white; text-decoration:none;"href="#reviews">Reviews</a></li>
+</ul>
+</div>
+</div>
+<div class="col-xl-2">
+<div style="width:250px height:350px">
+<h4 style="color:white; text-align:left;">Support </h4>
+<ul style="list-style-type:none; text-align:left;">
+<li><a style="color:white; text-decoration:none;"href="#home">FAQ</a></li>
+<li><a style="color:white; text-decoration:none;"href="#sinup">Help Desk</a></li>
+<li><a style="color:white; text-decoration:none;"href="#download">Fourms</a></li>
+</ul>
+</div>
+</div>
+<div class="col-xl-3">
+<div style="width:250px height:350px">
+<a href="#"><img width="30px" height="30px" src="twi.png"></a>
+<a href="#"><img width="30px" height="30px" src="fc.png"></a>
+<a href="#"><img width="30px" height="30px" src="gp.png"></a><br><br>
+<h6 style="color:white;">contact:<br>+91 123 456 7890</h6>
+<button style="border:none; background:white; border-radius:18px; padding:10px; margin-left:px;">Contact Us</button>
+<h6 style="color:white;">Address:<br>126-a street 120 foot rd.opp methis collection</h6>
+</div>
+</div>
+</div>
+<div class="col-12">
+<small style="color:white;"><img width="20px" height="20px" src="co.png">2023 copyrights All right reserved by aniket sharma</small>
+</div>
+</center>
+</div>
+</footer>
+<!-- Your rest of the content here -->
+<script>
+$(document).ready(function() {
+  // Function to check if the navbar needs to be scrolled
+  function checkScroll() {
+    var scrollPos = $(document).scrollTop();
+
+    if (scrollPos > 50) {
+      // If the user has scrolled down more than 50 pixels, add the "scrolled" class to the navbar
+      $('.navbar').addClass('scrolled');
+    } else {
+      // If the user has scrolled back to the top, remove the "scrolled" class from the navbar
+      $('.navbar').removeClass('scrolled');
+    }
+  }
+
+  // Call the checkScroll function on page load
+  checkScroll();
+
+  // Call the checkScroll function every time the user scrolls
+  $(window).on('scroll', function() {
+    checkScroll();
+  });
+});
+</script>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,214 @@
+<?php
+
+if(isset($_POST['save']))
+{
+	
+$email=$_POST['email'];
+
+$password=$_POST['password'];
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="travelvista";
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+
+
+if($conn->connect_error)
+{
+	die("connection failed".$conn->connect_error);
+}
+
+
+$sql="insert into login(email,password)values('$email','$password')";
+if($conn->query($sql)==True)
+{
+	?><h3 class="msg"><?php echo "Record inserted successfully";
+	
+	?></h3><?php
+	header("Location: afterindex.html");
+	
+}
+else
+{
+	?><h3 class="msg"><?php echo "Error".$sql."<br/>".$conn->error;?></h3><?php
+}
+$conn->close();
+}
+
+?>
+
+
+
+
+
+<html>
+<head>
+<title>login</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--<link rel="stylesheet" type="text/css" href="bootstrap.css">-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<style>
+a#vis {
+  color: white;
+}
+
+a#vis:hover {
+  color: powderblue;
+}
+.scrolled {
+  background-color: rgba(37, 125, 199, 1) !important;
+}
+</style>
+<body style="background-color:#257DC7">
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top" style="background:rgba(0,0,0,0);">
+<div class="container">
+<a href="#" class="navbar-brand"><img src="lg.png">TRAVEL VISTA</a>
+<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarid">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarid">
+<ul class="navbar-nav text-center ml-auto">
+<li class="nav-items">
+<a href="index.html" class="nav-link" id="vis">Home</a>
+</li>
+<li class="nav-items">
+<a href="about.html" class="nav-link" id="vis">About Us</a>
+</li>
+<li class="nav-items">
+<a href="servecies.html" class="nav-link" id="vis">Services</a>
+</li>
+
+
+<li class="nav-items">
+<a href="login.php" class="nav-link" id="vis">login</a>
+</li>
+<li class="nav-items">
+<a href="signup.php" class="nav-link" id="vis">signup</a>
+</li>
+<li class="nav-items">
+<a href="contact.php" class="nav-link" id="vis">Mail Us</a>
+</li>
+</ul>
+</div>
+</div>
+</nav><br><br><br>
+<div class="container">
+<div class="row">
+<div class="col-xl-8">
+<img src="simage/rt.jpg" class="img-fluid" style="margin-top:40px; border-radius:40px; box-shadow: -4px 4px 8px rgba(0, 0, 0, 0.9);">
+</div>
+<div class="col-xl-4">
+<div style="height:631px; width:400px; border-radius: 5px; background-color:#257DC7; margin:40px 10% 0% 0px%; padding:20px;">
+
+<img src="lg.png"><h1 style="text-align:center; color:white; margin-top:30px; ">Travel Vista</h1>
+<form method="POST" style="margin:90px 0px 0px 0px;">
+ <label for="email" style="color:white;">Email</label>
+  <input type="text" id="email" name="email" style="width: 100%; padding: 12px 20px; background-color:#257DC7; margin: 8px 0; box-sizing: border-box; border: none; border-bottom: 2px solid white;" required>
+  <label for="password" style="color:white;">password</label>
+  <input type="password" id="password" name="password" style="width: 100%; padding: 12px 20px; background-color:#257DC7; margin: 8px 0; box-sizing: border-box; border: none; border-bottom: 2px solid white;" required>
+  <a href="Login" style=" text-decoration:none; color:white;"><small>Forget your password?</small></a><br>
+  <label style="color: white;">
+  <input type="checkbox" name="remember" style="margin-right: 5px;">Remember Me
+  </label>
+  <center>
+   
+   <button type="save" name="save" class="btn btn-danger" style="width: 300px; background-color:white; ; color:#257DC7; padding: 14px 20px; margin: 20px 0; border: none; border-radius: 10px;">Submit</button>
+   </center>
+</form>
+</div>
+</div>
+</div>
+</div>
+<footer class="footer" style="background-color:#257DC7; margin-top:40px">
+<div class="container-fluid" style="padding-top:40px;">
+<center>
+<div class="row">
+<div class="col-xl-3">
+<div style="width:250px height:350px">
+<img width="200px" height="200px" src="logo.png">
+</div>
+</div>
+<div class="col-xl-2">
+<div style="width:250px height:350px">
+<h4 style="color:white; text-align:left;">Get Started</h4>
+<ul style="list-style-type:none; text-align:left;">
+<li><a style="color:white; text-decoration:none; hover=color:black;" href="navcol.html">Home</a></li>
+<li><a style="color:white; text-decoration:none;" href="formdiv.html">Sing Up</a></li>
+<li><a style="color:white; text-decoration:none;" href="#download">Download</a></li>
+</ul>
+</div>
+</div>
+<div class="col-xl-2">
+<div style="width:250px height:350px">
+<h4 style="color:white; text-align:left;">About Us</h4>
+<ul style="list-style-type:none; text-align:left;">
+<li><a style="color:white; text-decoration:none;"href="about.html">Company Information</a></li>
+<li><a style="color:white; text-decoration:none;"href="#contact Us">Contact Us</a></li>
+<li><a style="color:white; text-decoration:none;"href="#reviews">Reviews</a></li>
+</ul>
+</div>
+</div>
+<div class="col-xl-2">
+<div style="width:250px height:350px">
+<h4 style="color:white; text-align:left;">Support </h4>
+<ul style="list-style-type:none; text-align:left;">
+<li><a style="color:white; text-decoration:none;"href="#home">FAQ</a></li>
+<li><a style="color:white; text-decoration:none;"href="#sinup">Help Desk</a></li>
+<li><a style="color:white; text-decoration:none;"href="#download">Fourms</a></li>
+</ul>
+</div>
+</div>
+<div class="col-xl-3">
+<div style="width:250px height:350px">
+<a href="#"><img width="30px" height="30px" src="twi.png"></a>
+<a href="#"><img width="30px" height="30px" src="fc.png"></a>
+<a href="#"><img width="30px" height="30px" src="gp.png"></a><br><br>
+<button style="border:none; background:white; border-radius:18px; padding:10px; margin-left:px;">Contact Us</button>
+</div>
+</div>
+</div>
+<div class="col-12">
+<small style="color:white;"><img width="20px" height="20px" src="co.png">2023 copyrights All right reserved by aniket sharma</small>
+</div>
+</center>
+</div>
+</footer>
+<!-- Your rest of the content here -->
+<script>
+$(document).ready(function() {
+  // Function to check if the navbar needs to be scrolled
+  function checkScroll() {
+    var scrollPos = $(document).scrollTop();
+
+    if (scrollPos > 50) {
+      // If the user has scrolled down more than 50 pixels, add the "scrolled" class to the navbar
+      $('.navbar').addClass('scrolled');
+    } else {
+      // If the user has scrolled back to the top, remove the "scrolled" class from the navbar
+      $('.navbar').removeClass('scrolled');
+    }
+  }
+
+  // Call the checkScroll function on page load
+  checkScroll();
+
+  // Call the checkScroll function every time the user scrolls
+  $(window).on('scroll', function() {
+    checkScroll();
+  });
+});
+</script>
+</body>
+</html>
